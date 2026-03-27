0035caf68969c7a9c0145f1a5ed10d5981e2b4a7
gustavozequim <gustavozequim9@gmail.com>
2026-03-27 16:01:10 -0300

    Adiciona o setting_model e faz a listagem do ids dos emails da caixa de entrada

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from setting import USR, SENHA
 
 from imaplib import IMAP4_SSL
 
-conexao = IMAP4_SSL('imap.gmail.com')
-conexao.login(USR, SENHA)
-ids = conexao.list()
-print(ids)
-
+conexao = IMAP4_SSL('imap.gmail.com') # conexão com o servidor do gmail
+conexao.login(USR, SENHA) # login com o email e senha
+ids = conexao.list("INBOX")[1][0].decode().split(' ')[2].strip('"') # pega o id da caixa de entrada
+conexao.select('INBOX') # seleciona a caixa de entrada para realizar as operações
+status, data = conexao.search(None, 'ALL') # busca por todos os emails da caixa de entrada, o status é para verificar se a busca foi realizada com sucesso
+ids = data[0].split() # divide os ids dos emails
+print(f'Foram encontrados {len(ids)} emails.') # imprime a quantidade de emails encontrados
+print(status)
+conexao.logout() # desconecta do servidor
